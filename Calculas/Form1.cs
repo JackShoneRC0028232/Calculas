8412ae06af9d93239b92322c684a86b26afdd972
Jack Shone <rc0028232@student.riversidecollege.ac.uk>
2018-04-25 10:27:10 +0100

    Add project files.

---
+++ Calculas/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Calculas
+{
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            public double time;
+            public double voltage;
+            public double current;
+            public double voltageDerivative;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "CSV Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new data());
+                            string[] l = sr.ReadLine().Split(',');
+                            table.Last().time = double.Parse(l[0]);
+                            table.Last().voltage = double.Parse(l[1]);
+                            table.last().current = double.Parse(l[2]);
+                            table.last().voltageDerivative = double.Parse(l[3]);
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format.");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+            }
+        }
+    }
+}
